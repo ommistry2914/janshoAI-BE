e455a8a4ed1538c741acf8c411f7c59921dfa686
om mistry <ommistry2914@gmail.com>
2025-11-06 01:23:25 +0530

    change in env local and config

--- src/config/db/index.ts
+++ src/config/db/index.ts
@@ -7,7 +7,7 @@ export default {
   port: process.env.PORT || 5000,
   database_url: process.env.DATABASE_URL,
   node_env: process.env.NODE_ENV || 'development',
-  ACCESS_SECRET: process.env.ACCESS_SECRET,
-  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
-  GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+  ACCESS_SECRET: process.env.ACCESS_SECRET || "default_access_secret",
+  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET || "default_refresh_secret",
+  GEMINI_API_KEY: process.env.GEMINI_API_KEY || "api_key_missing",
 };
